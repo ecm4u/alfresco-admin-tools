1187318d2006383e592ec7ce57c45d6f66f27c08
Lutz Horn <lutz.horn@ecm4u.de>
2016-12-20 11:17:00 +0100

    initialer Import

---
+++ export-group-memberships.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3.5
+
+import argparse
+import json
+import requests
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument("tenant", type=str, nargs="?", default="default")
+parser.add_argument("username", type=str)
+parser.add_argument("password", type=str)
+parser.add_argument("url", type=str, default="http://alf-host/alfresco/service/api/groups")
+args = parser.parse_args()
+
+tenant = args.tenant
+url_base = args.url
+if args.tenant != "default":
+    username = args.username + "@" + args.tenant
+else:
+    username = args.username
+auth = (username, args.password)
+
+res_groups = requests.get(url_base, params={"shortNameFilter": "*"}, auth=auth)
+if res_groups.status_code == 200:
+    groups = res_groups.json()["data"]
+    for group in groups:
+        groupShortName = group["shortName"]
+        res_members = requests.get(url_base + "/" + groupShortName + "/children", auth=auth)
+        if res_members.status_code == 200:
+            members = res_members.json()
+            with open("{}.{}.members.json".format(tenant, groupShortName), "w") as w:
+                w.write(json.dumps(members))
---
+++ import-group-memberships.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3.5
+
+import argparse
+import json
+import requests
+
+parser = argparse.ArgumentParser()
+parser.add_argument("tenant", type=str, nargs="?", default="default")
+parser.add_argument("membersFile", type=str)
+parser.add_argument("username", type=str)
+parser.add_argument("password", type=str)
+parser.add_argument("url", type=str, default="http://alf-host/alfresco/service/api/groups")
+args = parser.parse_args()
+
+tenant = args.tenant
+url_base = args.url
+if args.tenant != "default":
+    username = args.username + "@" + args.tenant
+else:
+    username = args.username
+auth = (username, args.password)
+
+groupName = args.membersFile.split(".")[1]
+
+with open(args.membersFile) as f:
+    members = json.load(f)["data"]
+    for member in members:
+        if member["authorityType"] == "GROUP":
+            name = member["fullName"]
+        else:
+            name = member["shortName"]
+        url = url_base + "/" + groupName + "/children/" + name
+        print(url)
+        res = requests.post(url, json={}, auth=auth)
+        print(res)
